e0b88375bb496a9bad5e37179b7d3dbce0994a86
Adil errai <67393790+Adilerrai@users.noreply.github.com>
2025-03-05 13:57:30 +0000

    Update settings.py

--- PriseRendez/settings.py
+++ PriseRendez/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'appointment',
+    'PriseRendez.appointment',
         # Django Allauth
     'allauth',
     'allauth.account',
